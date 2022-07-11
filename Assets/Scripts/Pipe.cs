42a99686d7d8ed5c1972edbff424c1e46bb64ac2
Adam Graham <adam@zigurous.com>
2022-07-10 23:35:12 -0500

    Scale mario when entering/exiting pipes

--- Assets/Scripts/Pipe.cs
+++ Assets/Scripts/Pipe.cs
@@ -20,11 +20,12 @@ public class Pipe : MonoBehaviour
 
     private IEnumerator Enter(Transform player)
     {
-        player.GetComponent<Player>().enabled = false;
+        player.GetComponent<PlayerMovement>().enabled = false;
 
         Vector3 enteredPosition = transform.position + enterDirection;
+        Vector3 enteredScale = Vector3.one * 0.5f;
 
-        yield return MoveToPosition(player, enteredPosition);
+        yield return MoveToPosition(player, enteredPosition, enteredScale);
         yield return new WaitForSeconds(1f);
 
         Camera.main.GetComponent<SideScrolling>().SetUnderground(connection.position.y < 0f);
@@ -32,31 +33,35 @@ public class Pipe : MonoBehaviour
         if (exitDirection != Vector3.zero)
         {
             player.position = connection.position - exitDirection;
-            yield return MoveToPosition(player, connection.position + exitDirection);
+            yield return MoveToPosition(player, connection.position + exitDirection, Vector3.one);
         }
 
         player.position = connection.position;
-        player.GetComponent<Player>().enabled = true;
+        player.localScale = Vector3.one;
+        player.GetComponent<PlayerMovement>().enabled = true;
     }
 
-    private IEnumerator MoveToPosition(Transform player, Vector3 endPosition)
+    private IEnumerator MoveToPosition(Transform player, Vector3 endPosition, Vector3 endScale)
     {
         float elapsed = 0f;
         float duration = 1f;
 
         Vector3 startPosition = player.position;
+        Vector3 startScale = player.localScale;
 
         while (elapsed < duration)
         {
             float t = elapsed / duration;
 
             player.position = Vector3.Lerp(startPosition, endPosition, t);
+            player.localScale = Vector3.Lerp(startScale, endScale, t);
             elapsed += Time.deltaTime;
 
             yield return null;
         }
 
         player.position = endPosition;
+        player.localScale = endScale;
     }
 
 }
